12c5020af143fbcb633d09e67c9003c4d9217139
Laura Enria <enria.laura2@gmail.com>
2018-04-24 11:57:25 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,39 @@
+
+//1)
+const musicians = [
+	'John Lennon',
+	'Paul McCartney',
+	'George Harrison',
+	'Ringo Starr',
+];
+
+const instruments = [
+  "Guitar",
+  "Bass Guitar",
+  "Lead Guitar",
+  "Drums"
+];
+
+function theBeatlesPlay(musicians,instruments) {
+  let band = [];
+	for (let i = 0; i < musicians.length; i++) {
+	  band.push([musicians[i]] + ' plays ' + instruments[i]);
+	}
+    return band;
+}
+
+//2)
+function johnLennonFacts(array) {
+  let john = [];
+  let i = 0;
+  while (i<array.length) {
+    john.push(array[i] + '!!!');
+    i++
+  }
+  return john;
+}
+
+//3
+function iLoveTheBeatles() {
+
+}
